407d98be560231e291bf6aba0a1a3e19bb57a5b3
sharevb <sharevb@gmail.com>
2024-01-01 16:46:17 +0100

    Checkbox label margin

--- packages/composeverter-website/src/components/Checkbox.js
+++ packages/composeverter-website/src/components/Checkbox.js
@@ -1,7 +1,7 @@
 export default ({ label, value, onChange, style }) => {
     return (
         <label style={style}>
-            <input type="checkbox" checked={value} onChange={onChange} />
+            <input type="checkbox" checked={value} onChange={onChange} style={{ marginRight: '0.5em' }} />
             {label}
         </label>
     );
